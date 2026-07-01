a1e6591e163159527eed99a9d7e8d597441a6702
Akhilesh Mishra <akhileshmish300@yahoo.in>
2026-07-01 12:25:08 +0530

    database check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { accountRouter } from "./account";
 
 export const mainRouter = express.Router()
 
+console.log("Checking DB Env:", process.env.DATABASE_URL ? "EXISTS!" : "MISSING!");
 mainRouter.use("/user", userRouter)
 mainRouter.use("/account", accountRouter)
 
